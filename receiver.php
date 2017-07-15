3f35ebc3d0aaed312eef24a836504075cf601172
Mark Redekopp <redekopp@usc.edu>
2017-07-14 20:21:27 -0700

    Updated

--- receiver.php
+++ receiver.php
@@ -9,10 +9,19 @@ $payload = $HTTP_RAW_POST_DATA;
 $data = json_decode($payload);
 $payloadHash = hash_hmac($algo, $payload, $secret);
 
+$mfile = 'receiver_php.log';
+$mstring = "Got here 1\n";
+file_put_contents($mfile, $mstring);
+
 // if it's not sent from github, kill it.
 if ($hash !== $payloadHash) {
+  $mstring = "died\n";
+  file_put_contents($mfile, $mstring);
   die('Unauthorized');
 }
+
+$mstring = "Got here 2\n";
+file_put_contents($mfile, $mstring);
 
 if ($data->ref == 'refs/heads/deploy') {
         echo shell_exec('python receiver.py ' . $data->repository->full_name . ' ' . $data->pusher->email);
--- receiver.py
+++ receiver.py
@@ -8,6 +8,8 @@ from ConfigParser import SafeConfigParser
 import smtplib
 from email.mime.text import MIMEText
 
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 TIMESTAMP = int(time.time())
 EMAIL_FROM = 'USC CS Bot <peterzha@usc.edu>'
 logs = []
@@ -16,11 +18,13 @@ with open('github_account.json', 'r') as file:
 	j = json.loads(file.read())
 	githubUsername = j['username']
 	githubPassword = j['password']
-
+        
 with open('sites.json', 'r') as file:
 	sites = json.loads(file.read())
 
 def log(to_log):
+        print githubPassword
+        print to_log
 	to_log = to_log.replace(githubPassword, '[REDACTED]')
 	logs.append(to_log)
 	return to_log
@@ -45,9 +49,9 @@ def email(subject, body, recipient):
 	msg['From'] = EMAIL_FROM
 	msg['To'] = recipient
 
-	s = smtplib.SMTP('localhost')
-	s.sendmail(EMAIL_FROM, [recipient], msg.as_string())
-	s.quit()
+	#s = smtplib.SMTP('localhost')
+	#s.sendmail(EMAIL_FROM, [recipient], msg.as_string())
+	#s.quit()
 
 def replace_line_if_starts_with(filename, prefix, replacement):
 	with open(filename, 'r') as file:
@@ -61,18 +65,20 @@ def replace_line_if_starts_with(filename, prefix, replacement):
 	  file.writelines(data)
 
 
+logf = open("mark.log","w");
+logf.write("started");
 repo_fullname = sys.argv[1]
 repo_name = repo_fullname[repo_fullname.index('/')+1:]
 pusher_email = sys.argv[2]
-
 if repo_fullname in sites:
 	target = sites[repo_fullname]['target']
 
 	git_url = 'https://' + githubUsername + ':' + githubPassword + '@github.com/' + repo_fullname + '.git'
 	dir_name = 'temp/' + repo_name + str(TIMESTAMP)
-
+        logf.write("Attempting to clone\n")
+        logf.write(git_url + dir_name + "\n")
 	shell('git clone ' + git_url + ' ' + dir_name)
-
+        logf.write("cloned")
 	# if clone is successful
 	if os.path.isdir(dir_name):
 		# first update the url in config
@@ -80,27 +86,29 @@ if repo_fullname in sites:
 		replace_line_if_starts_with(dir_name + '/_config.yml', 'url:', "url: '" + newurl + "'\n")
 
 		# build
+
+                logf.write("\nAbout to build")
 		shell('jekyll build', dir_name)
+                logf.write("Built")
 
 		# clean old built directory, and move
 		shell('rm -rf ../md/' + target)
 		shell('cp -r ' + dir_name + '/_site ../md/' + target)
-
 		# Remove the temporary clone
 		shell('rm -rf ' + dir_name)
 
 		# send email
-		email('Website deployed for ' + repo_fullname,
-			'Successful Deploy!\n\nFull logs:\n\n' + '\n'.join(logs),
-			pusher_email)
-	else:
-		email('Clone unsuccessful for ' + repo_fullname,
-			'We tried to clone your repository ' + repo_fullname + ', but was unsuccessful. Did you give permission to the bot?\n\nFull logs:\n\n' '\n'.join(logs),
-			pusher_email)
+#		email('Website deployed for ' + repo_fullname,
+#			'Successful Deploy!\n\nFull logs:\n\n' + '\n'.join(logs),
+#			pusher_email)
+#	else:
+#		email('Clone unsuccessful for ' + repo_fullname,
+#			'We tried to clone your repository ' + repo_fullname + ', but was unsuccessful. Did you give permission to the bot?\n\nFull logs:\n\n' '\n'.join(logs),
+#			pusher_email)
 
-else:
-	email('Repository ' + repo_fullname + 'not registered',
-			'We tried to deploy your repository ' + repo_fullname + ', but was unsuccessful. Have you registered your site with the Markdown service?',
-			pusher_email)
-
+#else:
+#	email('Repository ' + repo_fullname + 'not registered',
+#			'We tried to deploy your repository ' + repo_fullname + ', but was unsuccessful. Have you registered your site with the Markdown service?',
+#			pusher_email)
+logf.close()
 print logs
